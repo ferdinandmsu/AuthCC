ade4e5021613572c36efff1ee30bc54e5d4a306e
ferdinandlb <loucro.bro@gmail.com>
2021-05-22 12:01:24 +0200

    improved performance

--- include/AuthCC.h
+++ include/AuthCC.h
@@ -25,118 +25,6 @@
 
 namespace auth {
     namespace details {
-        enum class Error {
-            SUCCESS,
-            INVALID_LICENSE,
-            EMAIL_USED,
-            INVALID_USERNAME,
-            INVALID_HWID,
-            INVALID_DETAILS,
-            HWID_UPDATED,
-            TIME_EXPIRED,
-            NOT_INITIALIZED,
-            NOT_LOGGEDIN,
-            UNKNOWN
-        };
-
-        enum class Switch {
-            DISABLED,
-            ENABLED
-        };
-
-        struct User {
-            std::string hwid;
-            std::string username;
-            std::string ip;
-            std::string variable;
-            std::string expiry_date;
-            std::string email;
-
-            unsigned int id;
-            int rank;
-
-            friend std::ostream &operator<<(std::ostream &os, const User &user) {
-                os << "User(HWID: '" << user.hwid << "', username: '" << user.username << "', "
-                   << "ip: '" << user.ip << "', variable: '" << user.variable << "', expiry_date: '"
-                   << user.expiry_date << "', rank: " << user.rank << ", id: " << user.id
-                   << ", email: '" << user.email << "'" ")";
-                return os;
-            }
-        };
-
-        struct License {
-            std::string token;
-            std::string used_by;
-
-            bool used;
-            int days;
-            int rank;
-
-            friend std::ostream &operator<<(std::ostream &os, const License &license) {
-                os << std::boolalpha << "License(token: '" << license.token << "', rank: '" << license.rank
-                   << "', used: '" << license.used << "', used_by: '" << license.used_by << "', days: '"
-                   << license.days << ")";
-                return os;
-            }
-        };
-
-        struct AppInfo {
-            Switch status;
-            Switch developer_mode;
-            Switch download_link;
-            Switch free_mode;
-            Switch login;
-            Switch register_;
-
-            std::string hash;
-            std::string version;
-            std::string name;
-
-            unsigned long users;
-
-            friend std::ostream &operator<<(std::ostream &os, const AppInfo &info) {
-                os << std::boolalpha << "AppInfo(status: " << static_cast<int>(info.status) << ", developermode: "
-                   << static_cast<int>(info.developer_mode) << ", download_link: "
-                   << static_cast<int>(info.download_link) << ", free_mode: "
-                   << static_cast<int>(info.free_mode) << ", login: "
-                   << static_cast<int>(info.login) << ", register: "
-                   << static_cast<int>(info.register_) << ", hash: '"
-                   << info.hash << "', version: " << info.version << ", name: '" << info.name << "', users: "
-                   << info.users << ")";
-                return os;
-            }
-        };
-
-
-        Switch toSwitch(const std::string &str) {
-            return str == "Enabled" ? Switch::ENABLED : Switch::DISABLED;
-        }
-
-        std::string errorMessage(const Error error) {
-            if (error == Error::SUCCESS)
-                return "";
-            else if (error == Error::UNKNOWN)
-                return "Unknown";
-            else if (error == Error::NOT_INITIALIZED)
-                return "Client is not initialized";
-            else if (error == Error::NOT_LOGGEDIN)
-                return "Client is not logged in";
-            else if (error == Error::TIME_EXPIRED)
-                return "The account is expired";
-            else if (error == Error::HWID_UPDATED)
-                return "Hwid was updated";
-            else if (error == Error::INVALID_DETAILS)
-                return "Invalid credentails";
-            else if (error == Error::INVALID_HWID)
-                return "Invalid hwid";
-            else if (error == Error::EMAIL_USED)
-                return "Email is used";
-            else if (error == Error::INVALID_USERNAME)
-                return "Invalid username";
-            else
-                return "Something went wrong while resolving the error";
-        }
-
         std::string exec(const char *cmd) {
             std::array<char, 128> buffer{};
             std::string result;
@@ -186,14 +74,6 @@ namespace auth {
             return strip(exec("hostname"), [](const char c) { return isspace(c); });
         }
 
-        std::string username() {
-#ifdef OS_LINUX
-            return strip(exec("whoami"), [](const char c) { return isspace(c); });
-#else
-            return strip(exec("echo %USERNAME%"), [](const char c) {return isspace(c);});
-#endif
-        }
-
         std::string getHardwareID() {
             std::string hwid;
 
@@ -205,6 +85,117 @@ namespace auth {
 
             return strip(hwid, [](const char c) { return std::isspace(c); });
         }
+    }
+
+    enum class Error {
+        SUCCESS,
+        INVALID_LICENSE,
+        EMAIL_USED,
+        INVALID_USERNAME,
+        INVALID_HWID,
+        INVALID_DETAILS,
+        HWID_UPDATED,
+        TIME_EXPIRED,
+        NOT_INITIALIZED,
+        NOT_LOGGEDIN,
+        UNKNOWN
+    };
+
+    enum Switch {
+        DISABLED,
+        ENABLED
+    };
+
+    struct User {
+        std::string hwid;
+        std::string username;
+        std::string ip;
+        std::string variable;
+        std::string expiry_date;
+        std::string email;
+
+        unsigned int id{};
+        int rank{};
+
+        friend std::ostream &operator<<(std::ostream &os, const User &user) {
+            os << "User(HWID: '" << user.hwid << "', username: '" << user.username << "', "
+               << "ip: '" << user.ip << "', variable: '" << user.variable << "', expiry_date: '"
+               << user.expiry_date << "', rank: " << user.rank << ", id: " << user.id
+               << ", email: '" << user.email << "'" ")";
+            return os;
+        }
+    };
+
+    struct License {
+        std::string token;
+        std::string used_by;
+
+        bool used;
+        int days;
+        int rank;
+
+        friend std::ostream &operator<<(std::ostream &os, const License &license) {
+            os << std::boolalpha << "License(token: '" << license.token << "', rank: '" << license.rank
+               << "', used: '" << license.used << "', used_by: '" << license.used_by << "', days: '"
+               << license.days << ")";
+            return os;
+        }
+    };
+
+    struct AppInfo {
+        Switch status;
+        Switch developer_mode;
+        Switch download_link;
+        Switch free_mode;
+        Switch login;
+        Switch register_;
+
+        std::string hash;
+        std::string version;
+        std::string name;
+
+        unsigned long users{};
+
+        friend std::ostream &operator<<(std::ostream &os, const AppInfo &info) {
+            os << std::boolalpha << "AppInfo(status: " << static_cast<int>(info.status) << ", developermode: "
+               << static_cast<int>(info.developer_mode) << ", download_link: "
+               << static_cast<int>(info.download_link) << ", free_mode: "
+               << static_cast<int>(info.free_mode) << ", login: "
+               << static_cast<int>(info.login) << ", register: "
+               << static_cast<int>(info.register_) << ", hash: '"
+               << info.hash << "', version: " << info.version << ", name: '" << info.name << "', users: "
+               << info.users << ")";
+            return os;
+        }
+    };
+
+    Switch toSwitch(const std::string &str) {
+        return str == "Enabled" ? ENABLED : DISABLED;
+    }
+
+    std::string errorMessage(const Error error) {
+        if (error == Error::SUCCESS)
+            return "";
+        else if (error == Error::UNKNOWN)
+            return "Unknown";
+        else if (error == Error::NOT_INITIALIZED)
+            return "Client is not initialized";
+        else if (error == Error::NOT_LOGGEDIN)
+            return "Client is not logged in";
+        else if (error == Error::TIME_EXPIRED)
+            return "The account is expired";
+        else if (error == Error::HWID_UPDATED)
+            return "Hwid was updated";
+        else if (error == Error::INVALID_DETAILS)
+            return "Invalid details";
+        else if (error == Error::INVALID_HWID)
+            return "Invalid hwid";
+        else if (error == Error::EMAIL_USED)
+            return "Email is used";
+        else if (error == Error::INVALID_USERNAME)
+            return "Invalid username";
+        else
+            return "Something went wrong while resolving the error";
     }
 
     class Client {
@@ -229,36 +220,36 @@ namespace auth {
             }
         }
 
-        details::Error userLogin(const std::string &username, const std::string &password) {
-            if (!is_initialized) return details::Error::NOT_INITIALIZED;
+        Error userLogin(const std::string &username, const std::string &password) {
+            if (!is_initialized) return Error::NOT_INITIALIZED;
 
             try {
                 auto json = request("login", {{"username", username},
                                               {"password", password}});
                 if (json["result"].get<std::string>() == "invalid_details")
-                    return details::Error::INVALID_DETAILS;
+                    return Error::INVALID_DETAILS;
                 else if (json["result"].get<std::string>() == "invalid_hwid")
-                    return details::Error::INVALID_HWID;
+                    return Error::INVALID_HWID;
                 else if (json["result"].get<std::string>() == "hwid_updated")
-                    return details::Error::HWID_UPDATED;
+                    return Error::HWID_UPDATED;
                 else if (json["result"].get<std::string>() == "time_expired")
-                    return details::Error::TIME_EXPIRED;
+                    return Error::TIME_EXPIRED;
                 else if (json["result"].get<std::string>() != "success")
-                    return details::Error::UNKNOWN;
+                    return Error::UNKNOWN;
 
                 parseUser(json);
-                return details::Error::SUCCESS;
+                return Error::SUCCESS;
 
             } catch (std::runtime_error &) {
-                return details::Error::UNKNOWN;
+                return Error::UNKNOWN;
             }
         }
 
-        details::Error userRegister(const std::string &username,
-                                    const std::string &email,
-                                    const std::string &password,
-                                    const std::string &license) {
-            if (!is_initialized) return details::Error::NOT_INITIALIZED;
+        Error userRegister(const std::string &username,
+                           const std::string &email,
+                           const std::string &password,
+                           const std::string &license) {
+            if (!is_initialized) return Error::NOT_INITIALIZED;
 
             try {
                 auto json = request("register",
@@ -268,23 +259,23 @@ namespace auth {
                                      {"license",  license}});
 
                 if (json["result"].get<std::string>() == "invalid_license")
-                    return details::Error::INVALID_LICENSE;
+                    return Error::INVALID_LICENSE;
                 else if (json["result"].get<std::string>() == "email_used")
-                    return details::Error::EMAIL_USED;
+                    return Error::EMAIL_USED;
                 else if (json["result"].get<std::string>() == "invalid_username")
-                    return details::Error::INVALID_USERNAME;
+                    return Error::INVALID_USERNAME;
                 else if (json["result"].get<std::string>() != "success")
-                    return details::Error::UNKNOWN;
+                    return Error::UNKNOWN;
 
-                return details::Error::SUCCESS;
-            } catch (std::runtime_error &) {
-                return details::Error::UNKNOWN;
+                return Error::SUCCESS;
+            } catch (...) {
+                return Error::UNKNOWN;
             }
         }
 
-        details::Error log(const std::string &msg) {
-            if (!is_initialized) return details::Error::NOT_INITIALIZED;
-            if (!is_loggedin) return details::Error::NOT_LOGGEDIN;
+        Error log(const std::string &msg) {
+            if (!is_initialized) return Error::NOT_INITIALIZED;
+            if (!is_loggedin) return Error::NOT_LOGGEDIN;
 
             try {
                 auto json = request("log",
@@ -293,36 +284,37 @@ namespace auth {
                                      {"username", user.username}});
 
                 if (json["result"].get<std::string>() != "success")
-                    return details::Error::UNKNOWN;
+                    return Error::UNKNOWN;
 
-                return details::Error::SUCCESS;
-            } catch (std::runtime_error &) {
-                return details::Error::UNKNOWN;
+                return Error::SUCCESS;
+            } catch (...) {
+                return Error::UNKNOWN;
             }
         };
 
-        const std::string &getVar(const std::string &key) {
+        [[nodiscard]]
+        const std::string &getVar(const std::string &key) const {
             return variables.at(key);
         }
 
         [[nodiscard]]
-        const details::AppInfo &getInfo() const noexcept {
+        const AppInfo &getInfo() const noexcept {
             return info;
         }
 
         [[nodiscard]]
-        const details::User &getUser() const noexcept {
+        const User &getUser() const noexcept {
             return user;
         }
 
     private:
         void parseInfo(const nlohmann::json &raw) {
-            info.status = details::toSwitch(raw["status"]);
-            info.free_mode = details::toSwitch(raw["freemode"]);
-            info.download_link = details::toSwitch(raw["downloadlink"]);
-            info.login = details::toSwitch(raw["login"]);
-            info.register_ = details::toSwitch(raw["register"]);
-            info.developer_mode = details::toSwitch(raw["developermode"]);
+            info.status = toSwitch(raw["status"]);
+            info.free_mode = toSwitch(raw["freemode"]);
+            info.download_link = toSwitch(raw["downloadlink"]);
+            info.login = toSwitch(raw["login"]);
+            info.register_ = toSwitch(raw["register"]);
+            info.developer_mode = toSwitch(raw["developermode"]);
 
             info.hash = raw["hash"];
             info.version = raw["version"];
@@ -374,8 +366,8 @@ namespace auth {
         bool is_initialized;
         bool is_loggedin;
 
-        details::User user;
-        details::AppInfo info;
+        User user;
+        AppInfo info;
     };
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,36 @@
 #include "../include/AuthCC.h"
 
 int main() {
+    // auth client
     auth::Client client("AID",
                         "APIKEY",
                         "SECRET");
 
     if (client.init()) {
-        auto login_result = client.userLogin("moderator1", "pass123");
+        auth::Error ec; // error code
 
-        if (login_result != auth::details::Error::SUCCESS) {
-            std::cout << "Login failed" << std::endl;
-            std::cout << "Error: " << auth::details::errorMessage(login_result);
-            exit(-1);
+        // try to register a new user
+        ec = client.userRegister("newuser", "mail@mail.com",
+                                                   "pass123","LICENSE");
+        if (ec != auth::Error::SUCCESS) {
+            std::cout << "Error: " << auth::errorMessage(ec);
+            exit(-1); // register failed
         }
 
+        if (ec = client.userLogin("newuser", "pass123");
+            ec != auth::Error::SUCCESS) {
+            std::cout << "Error: " << auth::errorMessage(ec);
+            exit(-1); // login failed
+        }
+
+        /* SUCCESSFULLY REGISTERED */
+        std::cout << "You are logged in!";
+
+        // log message to auth.gg dashboard
+        client.log("Hello World");
+
     } else {
+        // initialization failed
         std::cout << "Init failed" << std::endl;
     }
 
